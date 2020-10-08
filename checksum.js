bb5ed73e04b5255147445784bbe9f173c7d7487a
Mirzokhidd <50044566+Mirzokhidd@users.noreply.github.com>
2020-10-08 07:38:58 +0400

    Update checksum.js

--- checksum.js
+++ checksum.js
@@ -1,3 +1,8 @@
+//You can run the code through the terminal by specifying the path and running the text, "node java.ja"
+//Or you can just run it rhought the 'Run code' to output function of visual studious
+
+//-------------------------------------------------------------------------------------------------------------
+
 var checkSum = function(value) {
     var x = value;
     var y = Math.floor(x/256);
